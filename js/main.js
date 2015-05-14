1d5b382e2b4da13e1affb6ce786a4a40aea65076
marb <marb61a@gmail.com>
2015-05-14 18:14:44 +0000

    added next and prev and playlist

--- js/main.js
+++ js/main.js
@@ -3,6 +3,28 @@ var audio;
 //Hide Pause
 $('#pause').hide();
 
+initAudio($('#playlist li:first-child'));
+
+function initAudio(element){
+	var song = element.attr('song');
+	var title = element.text();
+	var cover = element.attr('cover');
+	var artist = element.attr('artist');
+	
+	//Create audio object
+	audio = new Audio('media/'+ song);
+	
+	// Audio information
+	$('.artist').text(artist);
+	$('.title').text(title);
+	
+	//Song cover
+	$('img.cover').attr('src','images/covers/'+cover);
+	
+	$('#playlist li').removeClass('active');
+	element.addClass('active');
+}
+
 
 // Button Group Functionality
 
@@ -27,9 +49,43 @@ $('#stop').click(function(){
 	audio.currentTime = 0;
 });
 
+//Next button
+$('#next').click(function(){
+	audio.pause();
+	var next = $('#playlist li.active').next();
+	if(next.length == 0){
+		next = $('#playlist li:first-child');
+	}
+	initAudio(next);
+	audio.play();
+	showDuration();
+});
+
+//Prev button
+$('#prev').click(function(){
+	audio.pause();
+	var prev = $('#playlist li.active').prev();
+	if(prev.length == 0){
+		prev = $('#playlist li:last-child');
+	}
+	initAudio(prev);
+	audio.play();
+	showDuration();
+});
 
 
-// Volume Slider
+
+//Playlist song click
+$('#playlist li').click(function(){
+	audio.pause();
+	initAudio($(this));
+	$('#play').hide();
+	$('#pause').show();
+	audio.play();
+	showDuration();
+});
+
+
 
 //Volume control
 $('#volume').change(function(){
@@ -38,7 +94,7 @@ $('#volume').change(function(){
 
 
 
-//Time/Duration Bar
+//Time & Duration Bar
 function showDuration(){
 	$(audio).bind('timeupdate',function(){
 		//Get hours and minutes
